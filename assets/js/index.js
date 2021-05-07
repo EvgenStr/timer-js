a860b01aba8c9433c415e01f69136b52599d45a8
EvgenStr <juve2910@gmail.com>
2021-05-07 21:36:43 +0300

    add resume timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,14 +17,13 @@ let interval = null;
 
 btnStart.addEventListener('click', startTimer);
 btnReset.addEventListener('click', resetTimer);
-btnPause.addEventListener('click', pauseTimer)
+btnPause.addEventListener('click', pauseTimer);
+btnResume.addEventListener('click', resumeTimer);
 
 function startTimer() {
   start = Date.now();
   current = 0;
   interval = null;
-
-  console.log(start)
   interval = setInterval(() => {
     current = Date.now() - start;
     setTime(current, timeElem);
@@ -34,8 +33,10 @@ function startTimer() {
   });
 }
 
-function pauseTimer() {
+function pauseTimer(e) {
   clearInterval(interval);
+  e.target.hidden = true;
+  btnResume.hidden = false;
 }
 function resetTimer() {
   clearInterval(interval);
@@ -43,15 +44,23 @@ function resetTimer() {
   start = null;
   current = null;
   btnStart.hidden = false;
+  btnResume.hidden = true;
+  btnPause.hidden = true;
   setTime(0, timeElem);
+}
+function resumeTimer(e) {
+  btnPause.hidden = false;
+  e.target.hidden = true;
+  let temp = current;
+
 }
 
 function setTime(time, elem) {
-  if (time === 0){
+  if (time === 0) {
     elem.innerText = `00:00:00.000`;
     return;
   }
-  const hours = Math.floor((time / (1000*60*60)) % 24)
+  const hours = Math.floor((time / (1000 * 60 * 60)) % 24)
   const minutes = Math.floor(time / 60000);
   const seconds = ((time % 60000) / 1000).toFixed(0);
   const milliseconds = time % 1000;
