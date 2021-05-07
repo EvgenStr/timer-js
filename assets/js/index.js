26c2e4365c4927baff5cd28d14afbebde2a1efad
EvgenStr <juve2910@gmail.com>
2021-05-07 18:49:29 +0300

    add start timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+const btnStart = document.getElementById('start');
+const time = document.querySelector('.time');
+
+
+btnStart.addEventListener('click', startTimer());
+
+ const startTimer = function(){
+  let start =  Date.now();
+  let current = 0;
+
+  function timer(){}
+  console.log(start)
+}
